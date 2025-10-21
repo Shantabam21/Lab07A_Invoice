20c2e3aed16c91b672869e166238996ce6f5db1c
askarbolot <kabylbekov07@gmail.com>
2025-10-20 22:00:21 -0400

    Second Commit

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class Invoice {
+    private ArrayList<LineItem> lineItems;
+
+
+    public void addItem(LineItem lineItem) {
+        lineItems.add(lineItem);
+    }
+
+    public double getTotalAmount() {
+        double total = 0;
+        for (LineItem item : lineItems) {}
+            total += item.getTotal();
+        return total;
+    }
+
+    public String getInvoiceDetails() {
+        StringBuilder sb = new StringBuilder();
+        for (LineItem item : items) {
+            sb.append(item.toString()).append("\n");
+        }
+        sb.append("Total Due: $" + getTotalAmount());
+        return sb.toString();
+    }
+    public ArrayList<LineItem> getLineItems() {
+        return lineItems;
+    }
+    public void setLineItems(ArrayList<LineItem> lineItems) {
+        this.lineItems = lineItems;
+    }
 }
